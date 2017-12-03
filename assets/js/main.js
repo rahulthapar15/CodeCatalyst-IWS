4ef73bd5094cf8ed8bc73bc55aa3f48bbdac7991
Rahul Thapar <rahthap@gmail.com>
2017-12-04 03:34:48 +0530

    00

---
+++ assets/js/main.js
@@ -0,0 +1,175 @@
+(function($) {
+
+	var settings = {
+
+		// Parallax background effect?
+			parallax: true,
+
+		// Parallax factor (lower = more intense, higher = less intense).
+			parallaxFactor: 20
+
+	};
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1800px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var $window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$footer = $('#footer'),
+			$main = $('#main');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Touch?
+			if (skel.vars.mobile) {
+
+				// Turn on touch mode.
+					$body.addClass('is-touch');
+
+				// Height fix (mostly for iOS).
+					window.setTimeout(function() {
+						$window.scrollTop($window.scrollTop() + 1);
+					}, 0);
+
+			}
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Footer.
+			skel.on('+medium', function() {
+				$footer.insertAfter($main);
+			});
+
+			skel.on('-medium !medium', function() {
+				$footer.appendTo($header);
+			});
+
+		// Header.
+
+			// Parallax background.
+
+				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+					if (skel.vars.browser == 'ie'
+					||	skel.vars.mobile)
+						settings.parallax = false;
+
+				if (settings.parallax) {
+
+					skel.on('change', function() {
+
+						if (skel.breakpoint('medium').active) {
+
+							$window.off('scroll.strata_parallax');
+							$header.css('background-position', 'top left, center center');
+
+						}
+						else {
+
+							$header.css('background-position', 'left 0px');
+
+							$window.on('scroll.strata_parallax', function() {
+								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+							});
+
+						}
+
+					});
+
+					$window.on('load', function() {
+						$window.triggerHandler('scroll');
+					});
+
+				}
+
+		// Main Sections: Two.
+
+			// Lightbox gallery.
+				$window.on('load', function() {
+
+					$('#two').poptrox({
+						caption: function($a) { return $a.next('h3').text(); },
+						overlayColor: '#2c2c2c',
+						overlayOpacity: 0.85,
+						popupCloserText: '',
+						popupLoaderText: '',
+						selector: '.work-item a.image',
+						usePopupCaption: true,
+						usePopupDefaultStyling: false,
+						usePopupEasyClose: false,
+						usePopupNav: true,
+						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
+					});
+
+				});
+
+	});
+
+})(jQuery);
+
+// --------------------------------------------------
+
+// Creating FIREBASE Reference
+var firebase_reference = firebase.database().ref();
+firebase_reference.child("Rahul").set({
+	Status : "Defaut Status"
+});
+
+
+
+//Create a Child with the name of the person Signed In as-
+// Rahul will be replaced by Google Sign In return username
+
+var refer_user = firebase.database().child("Rahul");
+
+//Sign In Using Firebase
+function SignIn(){
+	console.log("Sign in clicked");
+
+	$('#codecatalyst').hide();
+	$('#welcome').show(3000);
+	$('#welcomeNote').hide();
+	$('#stats').show(2500);
+	$('#two').show(2500);
+
+	// //Get value of default status and set to firebase
+	// var pStatus = document.getElementByName('aboutUser').value;
+	// console.log(pStatus);
+
+}
+
+//Edit the status
+
+function updateStatus(){
+	var status = document.getElementById('status').value;
+	document.getElementsByName("about")[0].value = "";
+	document.getElementsByName("about")[0].placeholder = status;
+
+	//Update Status to Firebase
+	firebase_reference.child("Rahul").set({
+		Status : status
+	});
+	// $('#editStatus').modal('hide');
+	console.log(status);
+}
