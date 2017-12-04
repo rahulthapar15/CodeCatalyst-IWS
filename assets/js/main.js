7515c1b4e86902d386bab174711819de3a1b779d
Rahul Thapar <rahthap@gmail.com>
2017-12-04 13:57:05 +0530

    final try

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,6 +134,16 @@ $(document).ready(function () {
 	$('#logout').hide();
 	$("#uploadBtn").hide();
 	$(".upload-group").hide();
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			// User is signed in.
+			var token = firebase.auth().currentUser.uid;
+			queryDatabase(token);
+		} else {
+			// No user is signed in.
+			window.location = "index.html";
+		}
+	});
 });
 
 
@@ -192,10 +202,7 @@ function showUserProfile(){
 	$('#two').show(2500);
 	$('#two').html(" <ul class='actions'> \
 	<li> <a data-toggle='modal' data-target='#uploadImage' class='button special icon fa fa-plus'>Add New</a></li > \
-	</ul> \
-	<div class='row'><article class='6u 12u$(xsmall) work-item'> \
-	<a href="+user.photoURL+"class='image fit thumb'><img src="+user.photoURL+" alt='Caption of photo'/></a> \
-	</article ></div>");
+	</ul>");
 	$('#logout').show(2500);
 }
 //Edit the status
@@ -254,6 +261,37 @@ function uploadFile(){
 
 
 	console.log("Image Uploaded");
+}
+
+function queryDatabase(token) {
+	firebase.database().ref('/Posts/').once('value').then(function (snapshot) {
+		var PostObject = snapshot.val();
+		var keys = Object.keys(PostObject);
+		var currentRow;
+		for (var i = 0; i < keys.length; i++) {
+			var currentObject = PostObject[keys[i]];
+			if (i % 3 == 0) {
+				currentRow = document.createElement("div");
+				$(currentRow).addClass("row");
+				$("#contentHolder").append(currentRow);
+			}
+			var col = document.createElement("div");
+			$(col).addClass("col-lg-4");
+			var image = document.createElement("img");
+			image.src = currentObject.url;
+			$(image).addClass("contentImage");
+			var p = document.createElement("p");
+			$(p).html(currentObject.caption);
+			$(p).addClass("contentCaption");
+			$(col).append(image);
+			$(col).append(p);
+			$(currentRow).append(col);
+			//create new row on every third entry
+			//col-lg-4
+		}
+		// ...
+	});
+
 }
 
 
