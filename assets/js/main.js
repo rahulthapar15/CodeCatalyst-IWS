1e6d53e99b022abc1d1a91a42fdf8221216319f7
Rahul Thapar <rahthap@gmail.com>
2017-12-04 11:52:39 +0530

    update username

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,7 +173,7 @@ function showUserProfile(){
 	$('#codecatalyst').hide();
 	$('#welcome').show(3000);
 	// $('#username').html("Welcome,<strong>"+user.displayName+"</strong>");
-	$('#welcome').html("<a class='image avatar'><img src="+user.photoURL+"alt='' /></a>< h1 id= 'username'>"+user.displayName +"</h1><br><br> \
+	$('#welcome').html("<a class='image avatar'><img src="+user.photoURL+"alt='' /></a><h1>Welcome,<br><strong>"+user.displayName +"</strong></h1><br><br> \
 			<section> \
 				<form method='post' action='#'> \
 					<div class='row uniform 50%'> \
