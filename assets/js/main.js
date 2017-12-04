ed421ebbb1af99639be8ad8f06a9a594566460e7
Rahul Thapar <rahthap@gmail.com>
2017-12-04 06:00:47 +0530

    hmm

--- assets/js/main.js
+++ assets/js/main.js
@@ -216,24 +216,22 @@ function uploadFile(){
 	// 3. Completion observer, called on successful completion
 	uploadTask.on('state_changed', function (snapshot) {
 		// Observe state change events such as progress, pause, and resume
-		// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-		var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-		console.log('Upload is ' + progress + '% done');
-		switch (snapshot.state) {
-			case firebase.storage.TaskState.PAUSED: // or 'paused'
-				console.log('Upload is paused');
-				break;
-			case firebase.storage.TaskState.RUNNING: // or 'running'
-				console.log('Upload is running');
-				break;
-		}
+		// See below for more detail
 	}, function (error) {
 		// Handle unsuccessful uploads
 	}, function () {
 		// Handle successful uploads on complete
 		// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+		var postKey = firebase.database().ref('Images/').push().key;
 		var downloadURL = uploadTask.snapshot.downloadURL;
-		console.log(downloadURL);
+		var updates = {};
+		var postData = {
+			url: downloadURL,
+			caption: $("#imageCaption").val(),
+			user: user.uid
+		};
+		updates['/Posts/' + postKey] = postData;
+		firebase.database().ref().update(updates);
 	});
 
 
